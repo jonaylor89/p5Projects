4d538149bd829797444576c743aea08c903fa9d1
Antonio Leuther <leuther@strato.de>
2018-10-08 07:51:59 +0200

    ES5 to ES6 Class

--- FlowField/Particle.js
+++ FlowField/Particle.js
@@ -1,45 +1,43 @@
-
-function Particle(){
-
+class Particle {
+  constructor() {
     this.pos = createVector(random(width), random(height));
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
     this.maxSpeed = 2;
+  }
 
-    this.update = function(){
-      this.pos.add(this.vel);
-      this.vel.add(this.acc);
-      this.vel.limit(this.maxSpeed);
-      this.acc.mult(0);
+  update() {
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed);
+    this.acc.mult(0);
 
-      this.edges();
-    }
+    this.edges();
+  }
 
-    this.follow = function(vectors){
-      let x = floor(this.pos.x / scl);
-      let y = floor(this.pos.y / scl);
-      let index = x + y * cols;
-      let force = vectors[index];
-      this.applyForce(force);
+  follow(vectors) {
+    let x = floor(this.pos.x / scl);
+    let y = floor(this.pos.y / scl);
+    let index = x + y * cols;
+    let force = vectors[index];
 
-    }
+    this.applyForce(force);
+  }
 
-    this.applyForce = function(force){
-      this.acc.add(force);
-    }
+  applyForce(force){
+    this.acc.add(force);
+  }
 
-    this.show = function(){
-      stroke(0, 5);
-      strokeWeight(7);
-      point(this.pos.x, this.pos.y);
-    }
+  show(){
+    stroke(0, 5);
+    strokeWeight(7);
+    point(this.pos.x, this.pos.y);
+  }
 
-    this.edges = function(){
-      if (this.pos.x > width) this.pos.x = 0;
-      if (this.pos.x < 0) this.pos.x = width;
-      if (this.pos.y > height) this.pos.y = 0;
-      if (this.pos.y < 0) this.pos.y = height;
-
-    }
-
+  edges(){
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = height;
+  }
 }
